bb6a8799c7da69c7ce36edee00e2c17e10efe9c5
Babur171 <baburali894@gmail.com>
2023-08-31 16:53:21 +0500

    new code pushed

--- server.js
+++ server.js
@@ -1,42 +1,10 @@
-const express = require("express");
-const app = express();
-const dataBase = require("./database/index");
+const http = require("http");
+const app = require("./app");
 const { PORT } = require("./config/index");
-const router = require("./routers/index");
-const errorHandler = require("./errorHandler/index");
-const passport = require("./social/passport");
-// const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const port = PORT;
 
-//dsadsad
+const port = PORT || 3000;
 
-app.use(cors());
-app.use(morgan("tiny"));
-app.use(
-  require("express-session")({
-    secret: "SDSDASDASDSAKDLKJASLKDJLASDJLASJDL",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-
-// Initialize Passport
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
-app.use(express.json());
-
-app.use(router);
-
-dataBase();
-
-app.use("/storage", express.static("storage"));
-app.use(errorHandler);
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log("this app is running on " + port);
 });
