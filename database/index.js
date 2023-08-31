45e2aaa79f85fe8e054e8db38a15e8857a3f1b42
Babur171 <baburali894@gmail.com>
2023-08-31 19:36:52 +0500

    fix new

--- database/index.js
+++ database/index.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const { MONOBBFILE } = require("../config/index");
 const dataBase = async () => {
   try {
-    await mongoose.connect(MONOBBFILE);
+    await mongoose.connect(MONOBBFILE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   } catch (err) {
     console.log("error", err);
   }
