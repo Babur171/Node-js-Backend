7b10e9054b958baeea205da1ce7aef13d7aaf9fc
Babur171 <baburali894@gmail.com>
2023-08-31 19:27:21 +0500

    fix new

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const router = require("./routers/index");
 const errorHandler = require("./errorHandler/index");
 const passport = require("./social/passport");
 const { PORT } = require("./config/index");
-const session = require("express-session");
 // const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -16,26 +15,14 @@ const port = PORT || 3000;
 
 app.use(cors());
 app.use(morgan("tiny"));
+app.use(
+  require("express-session")({
+    secret: "SDSDASDASDSAKDLKJLASLKDJLASDJLASJDL",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-// app.use(
-//   session({
-//     cookie: {
-//       secure: true,
-//       maxAge: 60000,
-//     },
-//     store: new RedisStore(),
-//     secret: "SDSDASDASDSAKDLKJLASLKDJLASDJLASJD",
-//     saveUninitialized: true,
-//     resave: false,
-//   })
-// );
-
-// app.use(function (req, res, next) {
-//   if (!req.session) {
-//     return next(new Error("Oh no")); //handle error
-//   }
-//   next(); //otherwise continue
-// });
 // Initialize Passport
 app.use(passport.initialize());
 app.use(passport.session());
@@ -47,14 +34,6 @@ app.use(express.json());
 app.use(router);
 
 dataBase();
-const MongoStore = require("connect-mongo")(session);
-
-app.use(
-  session({
-    secret: "foo",
-    store: new MongoStore(options),
-  })
-);
 
 app.use("/storage", express.static("storage"));
 app.use(errorHandler);
